1c5929104af2b59feab75eec2c9753811ece6c6b
Aldo Venegas <aldo94vp@MacBook-Pro-de-Aldo.local>
2020-03-02 16:13:30 -0600

    refactor: return subject asObservable in service

--- communication-with-service/src/app/components/component-two/component-two.component.ts
+++ communication-with-service/src/app/components/component-two/component-two.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-component-two',
@@ -8,11 +9,15 @@ import { DataService } from 'src/app/services/data.service';
 })
 export class ComponentTwoComponent implements OnInit {
   name: string;
+  subscription: Subscription;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
-    this.dataService.subjectData$.subscribe(v => this.name = v);
+    this.subscription = this.dataService.subjectData$.subscribe(v => this.name = v);
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
--- communication-with-service/src/app/services/data.service.ts
+++ communication-with-service/src/app/services/data.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject, Observable } from 'rxjs';
 
 @Injectable()
 export class DataService {
   subjectData$ = new Subject<string>();
+
   private _data: string;
   set data(value: string) {
     this._data = value;
     this.subjectData$.next(this.data);
   }
   get data() { return this._data; }
-  
-  constructor() {}
+
+  getData(): Observable<string> {
+    return this.subjectData$.asObservable();
+  }
 }
